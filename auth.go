40e187ed560d513452e130ce99df164fd83f1e84
Arshit <f20220331@pilani.bits-pilani.ac.in>
2023-09-11 18:14:02 +0530

    added customer and vendors

---
+++ auth.go
@@ -0,0 +1,146 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"golang.org/x/crypto/bcrypt"
+)
+
+type User struct {
+	// ID         int    `json:"id"`
+	First_name  string `json:"first_name"`
+	Last_name   string `json:"last_name"`
+	Username    string `json:"username"`
+	Email       string `json:"email"`
+	Password    string `json:"password"`
+	Is_customer bool   `json:"is_customer"`
+	Is_vendor   bool   `json:"is_vendor"`
+}
+
+// Pass db as a parameter to getUsers
+func getCustomers(c *gin.Context, db *sql.DB) {
+
+	c.Header("Content-Type", "application/json")
+	rows, err := db.Query("SELECT first_name, last_name, username, email FROM customers")
+	if err != nil {
+		log.Fatal(err)
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Internal Server Error"})
+		return
+	}
+	defer rows.Close()
+
+	var customers []User
+	for rows.Next() {
+		var a User
+		err := rows.Scan(&a.First_name, &a.Last_name, &a.Username, &a.Email)
+		if err != nil {
+			log.Fatal(err)
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Internal Server Error"})
+			return
+		}
+		customers = append(customers, a)
+	}
+	c.IndentedJSON(http.StatusOK, customers)
+}
+
+func createCustomer(c *gin.Context, db *sql.DB) {
+	c.Header("Content-Type", "application/json")
+	var newuser User
+	if err := c.BindJSON(&newuser); err != nil {
+		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": "Invalid request payload"})
+		return
+	}
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(newuser.Password), bcrypt.DefaultCost)
+	newuser.Is_customer = true
+	if err != nil {
+		log.Fatal(err)
+	}
+	stmt, err := db.Prepare("INSERT INTO customers (first_name, last_name, username, email, password) VALUES ($1, $2, $3, $4, $5)")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer stmt.Close()
+
+	if _, err := stmt.Exec(newuser.First_name, newuser.Last_name, newuser.Username, newuser.Email, hashedPassword); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "username or email already exists"})
+		return
+	}
+	stmt_2, err := db.Prepare("INSERT INTO users (first_name, last_name, username, email, password, is_customer) VALUES ($1, $2, $3, $4, $5, $6)")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer stmt_2.Close()
+
+	if _, err := stmt_2.Exec(newuser.First_name, newuser.Last_name, newuser.Username, newuser.Email, hashedPassword, newuser.Is_customer); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "username or email already exists"})
+		return
+	}
+
+	c.IndentedJSON(http.StatusCreated, newuser)
+}
+
+func createVendors(c *gin.Context, db *sql.DB) {
+	c.Header("Content-Type", "application/json")
+	var newuser User
+	if err := c.BindJSON(&newuser); err != nil {
+		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": "Invalid request payload"})
+		return
+	}
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(newuser.Password), bcrypt.DefaultCost)
+	newuser.Is_vendor = true
+	if err != nil {
+		log.Fatal(err)
+	}
+	stmt, err := db.Prepare("INSERT INTO vendors (first_name, last_name, username, email, password) VALUES ($1, $2, $3, $4, $5)")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer stmt.Close()
+
+	if _, err := stmt.Exec(newuser.First_name, newuser.Last_name, newuser.Username, newuser.Email, hashedPassword); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "username or email already exists"})
+		return
+	}
+	stmt_2, err := db.Prepare("INSERT INTO users (first_name, last_name, username, email, password, is_vendor) VALUES ($1, $2, $3, $4, $5, $6)")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer stmt_2.Close()
+
+	if _, err := stmt_2.Exec(newuser.First_name, newuser.Last_name, newuser.Username, newuser.Email, hashedPassword, newuser.Is_vendor); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "username or email already exists"})
+		return
+	}
+
+	c.IndentedJSON(http.StatusCreated, newuser)
+}
+
+func loginHandler(c *gin.Context, db *sql.DB) {
+	var loginRequest struct {
+		Username string `json:"username" binding:"required"`
+		Password string `json:"password" binding:"required"`
+	}
+
+	if err := c.BindJSON(&loginRequest); err != nil {
+		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": "Invalid request payload"})
+		return
+	}
+
+	var user User
+	err := db.QueryRow("SELECT * FROM users WHERE username = $1", loginRequest.Username).
+		Scan(&user.First_name, &user.Last_name, &user.Username, &user.Email, &user.Password, &user.Is_customer, &user.Is_vendor)
+
+	if err != nil {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid username"})
+		return
+	}
+
+	if err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(loginRequest.Password)); err != nil {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid username or password"})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{"message": "Login successful", "user": user})
+}
--- main.go
+++ main.go
@@ -3,24 +3,11 @@ package main
 import (
 	"database/sql"
 	"fmt"
-	"log"
-	"net/http"
 
 	"github.com/gin-gonic/gin"
 	_ "github.com/lib/pq"
-	"golang.org/x/crypto/bcrypt"
 )
 
-type User struct {
-	ID         int    `json:"id"`
-	First_name string `json:"first_name"`
-	Last_name  string `json:"last_name"`
-	Username   string `json:"username"`
-	Email      string `json:"email"`
-	Password   string `json:"password"`
-}
-
-// sets the constants for connecting to our database
 const (
 	host     = "localhost"
 	port     = 5432
@@ -48,11 +35,14 @@ func main() {
 
 	router := gin.Default()
 	// Pass db as a parameter to getUsers
-	router.GET("/users", func(c *gin.Context) {
-		getUsers(c, db)
+	router.GET("/customers", func(c *gin.Context) {
+		getCustomers(c, db)
 	})
-	router.POST("/users/register", func(c *gin.Context) {
-		createUser(c, db)
+	router.POST("/customers/register", func(c *gin.Context) {
+		createCustomer(c, db)
+	})
+	router.POST("/vendors/register", func(c *gin.Context) {
+		createVendors(c, db)
 	})
 	router.POST("/users/login", func(c *gin.Context) {
 		loginHandler(c, db)
@@ -60,80 +50,4 @@ func main() {
 
 	router.Run("localhost:8080")
 
-}
-
-// Pass db as a parameter to getUsers
-func getUsers(c *gin.Context, db *sql.DB) {
-
-	c.Header("Content-Type", "application/json")
-	rows, err := db.Query("SELECT id, first_name, last_name, username, email FROM users")
-	if err != nil {
-		log.Fatal(err)
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "Internal Server Error"})
-		return
-	}
-	defer rows.Close()
-
-	var users []User
-	for rows.Next() {
-		var a User
-		err := rows.Scan(&a.ID, &a.First_name, &a.Last_name, &a.Username, &a.Email)
-		if err != nil {
-			log.Fatal(err)
-			c.JSON(http.StatusInternalServerError, gin.H{"error": "Internal Server Error"})
-			return
-		}
-		users = append(users, a)
-	}
-	c.IndentedJSON(http.StatusOK, users)
-}
-
-func createUser(c *gin.Context, db *sql.DB) {
-	c.Header("Content-Type", "application/json")
-	var newuser User
-	if err := c.BindJSON(&newuser); err != nil {
-		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": "Invalid request payload"})
-		return
-	}
-	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(newuser.Password), bcrypt.DefaultCost)
-	if err != nil {
-		log.Fatal(err)
-	}
-	stmt, err := db.Prepare("INSERT INTO users (id, first_name, last_name, username, email, password) VALUES ($1, $2, $3, $4, $5, $6)")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer stmt.Close()
-
-	if _, err := stmt.Exec(newuser.ID, newuser.First_name, newuser.Last_name, newuser.Username, newuser.Email, hashedPassword); err != nil {
-		log.Fatal(err)
-	}
-	c.IndentedJSON(http.StatusCreated, newuser)
-}
-
-func loginHandler(c *gin.Context, db *sql.DB) {
-	var loginRequest struct {
-		Username string `json:"username" binding:"required"`
-		Password string `json:"password" binding:"required"`
-	}
-
-	if err := c.BindJSON(&loginRequest); err != nil {
-		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": "Invalid request payload"})
-		return
-	}
-
-	var user User
-	err := db.QueryRow("SELECT * FROM users WHERE username = $1", loginRequest.Username).
-		Scan(&user.ID, &user.First_name, &user.Last_name, &user.Username, &user.Email, &user.Password)
-
-	if err != nil {
-		c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid username or password"})
-		return
-	}
-
-	if err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(loginRequest.Password)); err != nil {
-		c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid username or password"})
-		return
-	}
-	c.JSON(http.StatusOK, gin.H{"message": "Login successful", "user": user})
 }
